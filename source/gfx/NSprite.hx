e2ee0c8272c8664ae962cc1949746383e6ffc100
nomnom-dv <149065490+Noam-dv@users.noreply.github.com>
2023-11-16 17:33:27 +0200

    GFX imports

--- source/gfx/NSprite.hx
+++ source/gfx/NSprite.hx
@@ -1,6 +1,8 @@
 package gfx;
 
 import flixel.FlxSprite;
+import flash.display.BitmapData;
+import flixel.graphics.*;
 
 class NSprite extends FlxSprite implements InGFXobj
 {
@@ -14,15 +16,16 @@ class NSprite extends FlxSprite implements InGFXobj
     public dynamic function postUpdate(elapsed:Float) {}
 
     public var __graphic__(get,default):FlxGraphic;
-    function get___graphic__(){ return this.SimpleGraphicDefault}
+    public function get___graphic__(){ return this.SimpleGraphicDefault}
 
-    
+    public var SimpleGraphicDefault:FlxGraphicAsset;
     override function update(elapsed:Float) {
         preUpdate(elapsed);
         super.update(elapsed);
         postUpdate(elapsed);
     }
     public function new(x:Float,y:Float,?g:Null<FlxGraphicAsset>) {
+        this.SimpleGraphicDefault = g;
         //__graphic__= g != null ? g : new BitmapData(500,500,false,0xFFFFFFFF).fromImage("haxe/default-byte-logo.png");
     }
     public function addAnim(n:String = "d", p:String = "d", fps:Int = 24, loop:Bool = false){ this.animation.addByPrefix(n,p,fps,loop); }
