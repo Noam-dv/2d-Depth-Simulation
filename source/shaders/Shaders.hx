3205c2041933025b1e1cc8de732a9ceade9550ff
nomnom-dv <149065490+Noam-dv@users.noreply.github.com>
2023-11-21 09:29:54 +0200

    added my utility stuff

--- source/shaders/Shaders.hx
+++ source/shaders/Shaders.hx
@@ -5,6 +5,21 @@ import shaders.FlxRuntimeShader;
 
 class Shaders
 {
+    public static var p(get,default):String;
+    public static function get_p():String {
+        return "
+        #pragma header
+
+		vec2 uv = openfl_TextureCoordv.xy;
+		vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;
+		vec2 iResolution = openfl_TextureSize;
+		uniform float iTime;
+		#define iChannel0 bitmap
+		#define texture flixel_texture2D
+		#define fragColor gl_FragColor
+		#define mainImage main
+        ";
+    }
 	public static function downscale():FlxRuntimeShader
 	{
 		return new FlxRuntimeShader("
