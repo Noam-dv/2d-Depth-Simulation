f83ed10be876d7d66b7cff1f0467610ca3845915
nomnom-dv <149065490+Noam-dv@users.noreply.github.com>
2023-11-23 13:40:24 +0200

    roomdata to floor

--- source/backend/FloorFile.hx
+++ source/backend/FloorFile.hx
@@ -1,8 +1,9 @@
 package backend;
 
-import haxe.Json;
-import sys.io.File;
 import entitys.GameData;
+import haxe.Json;
+import sys.FileSystem;
+import sys.io.File;
 
 enum FloorItems
 {
@@ -10,50 +11,59 @@ enum FloorItems
 	ENEMY;
 	ITEM;
 }
-class FloorFile {
-    public static function loadFloorFromFile(filePath:String):Map<Int, Map<FloorItems, Array<Dynamic>>> {
-        var floorData:Map<Int, Map<FloorItems, Array<Dynamic>>> = new Map<Int, Map<FloorItems, Array<Dynamic>>>();
 
-        var fileContent:String = File.getContent(Paths.data("test", Json_File));
-        var parsedDynamic:Dynamic = Json.parse(fileContent);
+class FloorFile
+{
+	public static function loadFloorFromFile(filePath:String):Map<Int, Map<FloorItems, Array<Dynamic>>>
+	{
+		var floorData:Map<Int, Map<FloorItems, Array<Dynamic>>> = new Map<Int, Map<FloorItems, Array<Dynamic>>>();
 
-        var jsonData:FloorJSON = new FloorJSON();
-        jsonData.rooms = [];
+		var fileContent:String = File.getContent(filePath);
+		var parsedDynamic:Dynamic = Json.parse(fileContent);
 
-        if (parsedDynamic != null && Reflect.hasField(parsedDynamic, "rooms")) {
-            var rooms:Array<Dynamic> = Reflect.field(parsedDynamic, "rooms");
+		if (parsedDynamic != null && Reflect.hasField(parsedDynamic, "rooms"))
+		{
+			for (room in Reflect.field(parsedDynamic, "rooms"))
+			{
+				var roomData:RoomData = parseRoomData(room);
 
-            for (room in rooms) {
-                var roomData:RoomData = new RoomData(room.id);
+				var enemyData:Array<Dynamic> = Reflect.field(room, "enemies");
+				var itemData:Array<Dynamic> = Reflect.field(room, "items");
 
-                if (Reflect.hasField(room, "enemies")) {
-                    var enemies:Array<Dynamic> = Reflect.field(room, "enemies");
-                    for (enemy in enemies) {
-                        var enemyData:EnemyData = new EnemyData(enemy.type, enemy.x, enemy.y);
-                        roomData.enemies.push(enemyData);
-                    }
-                }
+				if (enemyData != null)
+					roomData.enemies = parseEnemyOrItemData(enemyData, EnemyData);
+				if (itemData != null)
+					roomData.items = parseEnemyOrItemData(itemData, ItemData);
 
-                if (Reflect.hasField(room, "items")) {
-                    var items:Array<Dynamic> = Reflect.field(room, "items");
-                    for (item in items) {
-                        var itemData:ItemData = new ItemData(item.type, item.x, item.y);
-                        roomData.items.push(itemData);
-                    }
-                }
+				floorData.set(roomData.id, roomDataToMap(roomData));
+			}
+		}
 
-                jsonData.rooms.push(roomData);
-                floorData.set(roomData.id, roomDataToMap(roomData));
-            }
-        }
+		return floorData;
+	}
 
-        return floorData;
-    }
+	private static function parseRoomData(room:Dynamic):RoomData
+	{
+		var roomData:RoomData = new RoomData(room.id);
+		return roomData;
+	}
 
-    private static function roomDataToMap(roomData:RoomData):Map<FloorItems, Array<Dynamic>> {
-        var map:Map<FloorItems, Array<Dynamic>> = new Map<FloorItems, Array<Dynamic>>();
-        map.set(FloorItems.ENEMY, roomData.enemies);
-        map.set(FloorItems.ITEM, roomData.items);
-        return map;
-    }
+	private static function parseEnemyOrItemData(data:Array<Dynamic>, DataType:Class<Dynamic>):Array<Dynamic>
+	{
+		var dataArray:Array<Dynamic> = [];
+		for (item in data)
+		{
+			var parsedData:Dynamic = cast(item, DataType);
+			dataArray.push(parsedData);
+		}
+		return dataArray;
+	}
+
+	private static function roomDataToMap(roomData:RoomData):Map<FloorItems, Array<Dynamic>>
+	{
+		var map:Map<FloorItems, Array<Dynamic>> = new Map<FloorItems, Array<Dynamic>>();
+		map.set(FloorItems.ENEMY, roomData.enemies);
+		map.set(FloorItems.ITEM, roomData.items);
+		return map;
+	}
 }
